416bb4883652e911273ecc6b3d859a0c0c06f258
richard <glennie@glennies.co.uk>
2020-12-10 15:19:49 +0000

    accidental commit undone

--- R/hmm.R
+++ R/hmm.R
@@ -370,7 +370,7 @@ HMM <- R6Class(
       
       # Data for TMB
       tmb_dat <- list(ID = self$obs()$data()$ID,
-                      data = as.matrix(self$obs()$obs_var(expand = TRUE)),
+                      data = as.matrix(self$obs()$obs_var()),
                       n_states = n_states,
                       distcode = distcode,
                       X_fe_obs = X_fe_obs,
