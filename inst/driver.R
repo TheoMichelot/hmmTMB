0eb6fd353b4ec54ccfb7ce792443a77c11865b82
richard <glennie@glennies.co.uk>
2020-12-12 01:32:13 +0000

    updated driver

--- inst/driver.R
+++ inst/driver.R
@@ -2,55 +2,13 @@
 
 library(hmmTMB)
 
-#' Simulate Poisson HMM
-#'
-#' @param n number of samples
-#' @param lambda means of Poisson for each state
-#' @param tpm transition probability matrix
-#' @param n.states number of states
-#' @param delta initial distribution
-#'
-#' @return vector of observed counts
-SimulatePoHmm <- function(n, lambda, tpm, n.states) {
-  delta <- solve(t(diag(n.states) - tpm  + 1), rep(1, n.states))
-  s <- numeric(n)
-  state.space <- 1:n.states
-  s <- sample(state.space, 1, prob = delta)
-  for (t in 2:n) s[t] <- sample(state.space, 1, prob = tpm[s[t - 1], ])
-  data <- rpois(n, lambda = lambda[s])
-  return(list(data = data, state = s))
-}
-
+# simulate data 
 n <- 10000
-lambda <- c(2, 20)
-tpm <- matrix(c(0.95, 0.1, 0.05, 0.9), nc = 2)
-n.states <- 2
+empty <- data.frame(ID = 1, count = rep(0, n))
+true_mod <- HMM$new("inst/true_mod.hmm")
+dat <- true_mod$simulate(n)
 
-simdat <- SimulatePoHmm(n, lambda, tpm, n.states)
-counts <- simdat$data
-states <- simdat$state
-
-# create objects
-dat <- data.frame(ID = 1,
-                  count = counts)
-dists <- list(count = "pois")
-par <- list(count = list(lambda = c(3, 6)))
-obs <- Observation$new(data = dat, dists = dists, n_states = 2, par = par)
-hid <- MarkovChain$new(n_states = 2)
-mod <- HMM$new(obs, hid)
-
-#fit model
-mod$fit(silent = FALSE)
+# fit model 
+mod <- HMM$new("inst/mod.hmm")
+mod$fit()
 mod$par()
-
-s <- mod$viterbi()
-table(s == states)/length(s)
-
-#### ALTERNATIVE 
-
-other_mod <- HMM$new("inst/mod.hmm")
-other_mod$fit() 
-other_mod$par() 
-
-
-
