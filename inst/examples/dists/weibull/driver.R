e79baf7b994583d0e09aed79350f3312207b98a3
richard <glennie@glennies.co.uk>
2021-01-12 14:19:45 +0000

    simple example for each distribution

---
+++ inst/examples/dists/weibull/driver.R
@@ -0,0 +1,27 @@
+
+library(hmmTMB)
+
+# Simulate data -----------------------------------------------------------
+
+# number of time steps
+n <- 1000
+
+# create an empty dataset
+dat <- data.frame(ID = rep(0, n), y = rep(0, n))
+
+# create true model 
+true_mod <- HMM$new(file = "true_mod.hmm")
+
+# simulate from true model
+dat <- true_mod$simulate(n)
+
+# Fit model ------------------------------------------------------------
+
+# create model to fit 
+mod <- HMM$new(file = "mod.hmm")
+
+# fit model
+mod$fit()
+
+# look at numerical estimates
+mod$par()
