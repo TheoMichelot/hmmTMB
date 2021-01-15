c849faa66998e6127ac9e313de79089dacdc2dcb
Théo Michelot <tm75@st-andrews.ac.uk>
2021-01-15 16:08:05 +0000

    add tests for make_formulas + move update.HMM tests to test_hmm.R

--- tests/testthat/test_hmm.R
+++ tests/testthat/test_hmm.R
@@ -41,3 +41,40 @@ test_that("edf works in case with no smooth", {
     hmm <- HMM$new(obs = obs, hidden = hid)
     expect_equal(hmm$edf(), length(hid$coeff_fe()) + length(obs$coeff_fe()))
 })
+
+# Create dummy data
+data <- data.frame(z = rnorm(100), x1 = rnorm(100), x2 = rnorm(100))
+# MarkovChain object
+hid <- MarkovChain$new(n_states = 2, structure = ~x1, data = data)
+# Observation object
+dists <- list(z = "norm")
+par <- list(z = list(mean = c(0, 1), sd = c(0.5, 2)))
+formulas <- list(z = list(mean = ~x1, sd = ~x2))
+obs <- Observation$new(data = data, dists = dists, n_states = 2, 
+                       par = par, formulas = formulas)
+# HMM object
+hmm <- HMM$new(obs = obs, hidden = hid)
+
+test_that("update.HMM modifies formulas correctly", {
+    # Update in hidden state model
+    hmm2 <- update(hmm, type = "hidden", i = 2, j = 1, 
+                   change = ~ . + I(x2^2), fit = FALSE)
+    
+    expect_equal(hmm2$hidden()$structure()[1,2], "~x1")
+    expect_equal(hmm2$hidden()$structure()[2,1], "~x1 + I(x2^2)")
+    expect_equal(hmm2$hidden()$formulas()[[1]], ~x1)
+    expect_equal(hmm2$hidden()$formulas()[[2]], ~x1 + I(x2^2))
+    
+    # Update in observation model
+    hmm3 <- update(hmm, type = "obs", i = "z", j = "mean", 
+                   change = ~ . - x1 + state1(x2), fit = FALSE)
+    hmm3 <- update(hmm3, type = "obs", i = "z", j = "sd", 
+                   change = ~ . + s(x1, k = 5, bs = 'cs'), fit = FALSE)
+    
+    expect_equal(hmm3$obs()$formulas()$z$mean$state1, ~1 + x2)
+    expect_equal(hmm3$obs()$formulas()$z$mean$state2, ~1)
+    expect_equal(hmm3$obs()$formulas()$z$sd$state1, 
+                 ~1 + x2 + s(x1, k = 5, bs = "cs"))
+    expect_equal(hmm3$obs()$formulas()$z$sd$state2, 
+                 ~1 + x2 + s(x1, k = 5, bs = "cs"))
+})
--- tests/testthat/test_misc.R
+++ tests/testthat/test_misc.R
@@ -1,43 +1,27 @@
 
-# Tests to add:
-# update.HMM updates the correct formula in the correct way
-# check format of output of make_formulas
-
 context("Miscellaneous")
 
-# Create dummy data
-data <- data.frame(z = rnorm(100), x1 = rnorm(100), x2 = rnorm(100))
-# MarkovChain object
-hid <- MarkovChain$new(n_states = 2, structure = ~x1, data = data)
-# Observation object
-dists <- list(z = "norm")
-par <- list(z = list(mean = c(0, 1), sd = c(0.5, 2)))
-formulas <- list(z = list(mean = ~x1, sd = ~x2))
-obs <- Observation$new(data = data, dists = dists, n_states = 2, 
-                       par = par, formulas = formulas)
-# HMM object
-hmm <- HMM$new(obs = obs, hidden = hid)
-
-test_that("update.HMM modifies formulas correctly", {
-    # Update in hidden state model
-    hmm2 <- update(hmm, type = "hidden", i = 2, j = 1, 
-                   change = ~ . + I(x2^2), fit = FALSE)
+test_that("make_formulas returns correct output", {
+    # Define state-specific formulas
+    f1 <- ~ state1(x1) + x2
+    f2 <- ~ state1(x1) + state2(s(x2, bs = "cs"))
+    input_forms <- list(step = list(shape = f1), count = list(lambda = f2))
     
-    expect_equal(hmm2$hidden()$structure()[1,2], "~x1")
-    expect_equal(hmm2$hidden()$structure()[2,1], "~x1 + I(x2^2)")
-    expect_equal(hmm2$hidden()$formulas()[[1]], ~x1)
-    expect_equal(hmm2$hidden()$formulas()[[2]], ~x1 + I(x2^2))
+    f <- make_formulas(input_forms = input_forms,
+                       var_names = c("step", "count"),
+                       par_names = list(step = c("shape", "scale"), count = "lambda"),
+                       n_states = 2)
     
-    # Update in observation model
-    hmm3 <- update(hmm, type = "obs", i = "z", j = "mean", 
-                   change = ~ . - x1 + state1(x2), fit = FALSE)
-    hmm3 <- update(hmm3, type = "obs", i = "z", j = "sd", 
-                   change = ~ . + s(x1, k = 5, bs = 'cs'), fit = FALSE)
+    # Check format
+    expect_equal(length(f), length(input_forms))
+    expect_equal(unname(sapply(f, length)), c(2, 1))
     
-    expect_equal(hmm3$obs()$formulas()$z$mean$state1, ~1 + x2)
-    expect_equal(hmm3$obs()$formulas()$z$mean$state2, ~1)
-    expect_equal(hmm3$obs()$formulas()$z$sd$state1, 
-                 ~1 + x2 + s(x1, k = 5, bs = "cs"))
-    expect_equal(hmm3$obs()$formulas()$z$sd$state2, 
-                 ~1 + x2 + s(x1, k = 5, bs = "cs"))
+    # Check formulas in output
+    f <- unlist(f)
+    expect_equal(f[[1]], ~1 + x1 + x2)
+    expect_equal(f[[2]], ~1 + x2)
+    expect_equal(f[[3]], ~1)
+    expect_equal(f[[4]], ~1)
+    expect_equal(f[[5]], ~1 + x1)
+    expect_equal(f[[6]], ~1 + s(x2, bs = "cs"))    
 })
