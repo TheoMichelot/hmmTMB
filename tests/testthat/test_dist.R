ec11c8a579ce9969174c4d0c6d6613129fa4bea3
Théo Michelot <tm75@st-andrews.ac.uk>
2021-01-15 10:47:53 +0000

    update unit tests for Dist

--- tests/testthat/test_dist.R
+++ tests/testthat/test_dist.R
@@ -1,6 +1,27 @@
 
-# Tests to add:
-# check that n2w and w2n cancel out (and return right format)
-# check pdf_apply against base R functions (e.g., dist_norm$pdf_apply again dnorm, etc.)
-
 context("Dist class")
+
+test_that("n2w and w2n return right format (dist_norm)", {
+    par <- list(mean = c(-5, 5), sd = c(2, 10))
+    wpar <- dist_norm$n2w(par = par)
+    expect_is(wpar, "numeric")
+    expect_equal(length(wpar), length(unlist(par)))
+
+    par2 <- dist_norm$w2n(wpar = wpar)
+    expect_is(par2, "list")
+    expect_equal(length(par2), length(par))
+    
+    par3 <- dist_norm$w2n(wpar = wpar, as_matrix = TRUE)
+    expect_is(par3, "matrix")
+    expect_equal(dim(par3), c(length(par$mean), length(par)))
+    expect_equal(colnames(par3), names(par))
+})
+
+test_that("pdf_apply returns the right output (dist_norm)", {
+    x <- pi
+    par <- c(mean = 1, sd = 5)
+    expect_equal(dist_norm$pdf_apply(x = x, par = par, log = FALSE),
+                 dnorm(x, mean = par[1], sd = par[2], log = FALSE))
+    expect_equal(dist_norm$pdf_apply(x = x, par = par, log = TRUE),
+                 dnorm(x, mean = par[1], sd = par[2], log = TRUE))
+})
