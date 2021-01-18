5ac1832821c031be25c92264947af79e938c3730
Théo Michelot <tm75@st-andrews.ac.uk>
2021-01-18 14:33:43 +0000

    add documentation for package itself (i.e., ?hmmTMB)

---
+++ R/hmmTMB-package.R
@@ -0,0 +1,13 @@
+
+# Documentation that comes up when calling ?hmmTMB
+# From roxygen2 documentation: "Package documentation will automatically 
+# include information parsed from the DESCRIPTION, including title, 
+# description, list of authors, and useful URLs."
+
+#' @details
+#' The package hmmTMB is based on three main classes (i.e., types of objects):
+#' MarkovChain, Observation, and HMM. Type ?MarkovChain, ?Observation or ?HMM
+#' to find their documentation, or consult the package vignettes for detailed
+#' examples of the hmmTMB workflow.
+#' @keywords internal
+"_PACKAGE"
