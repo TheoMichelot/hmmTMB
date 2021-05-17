72ed193312eda1ba7867fbef9be7ff9c0c07d84d
richard <glennie@glennies.co.uk>
2021-05-17 16:29:48 +0100

    Added simple dist checks.

--- tests/testthat/test_dist.R
+++ tests/testthat/test_dist.R
@@ -1,6 +1,20 @@
 
 context("Dist class")
 
+test_that("Distribution can be accessed and correct information is returned", {
+    obj <- dist_norm
+    expect_equal(obj$name(), "norm")
+    expect_equal(obj$pdf(), dnorm)    
+    expect_equal(obj$rng(), rnorm)
+    expect_equal(obj$link(), list(mean = identity, sd = log))
+    expect_equal(obj$invlink(), list(mean = identity, sd = exp))
+    expect_equal(obj$npar(), 2)
+    expect_equal(obj$parnames(), c("mean", "sd"))
+    expect_equal(obj$parapprox(), function(x) {return(c(mean(x), sd(x)))})
+    expect_equal(obj$fixed(), c(FALSE, FALSE))
+    expect_equal(obj$code(), 1)
+})
+
 test_that("n2w and w2n return right format (dist_norm)", {
     par <- list(mean = c(-5, 5), sd = c(2, 10))
     wpar <- dist_norm$n2w(par = par)
