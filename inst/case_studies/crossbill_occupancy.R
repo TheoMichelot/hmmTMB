26fc17121f5af7c73cb30986ffee2491f6ed50f5
richard <glennie@glennies.co.uk>
2021-06-04 14:42:37 +0100

    crossbill occupancy example

--- inst/case_studies/crossbill_occupancy.R
+++ inst/case_studies/crossbill_occupancy.R
@@ -11,6 +11,7 @@ data(crossbill, package = "unmarked")
 # get into format for hmmTMB
 nsites <- nrow(crossbill)
 dat <- data.frame(ID = rep(1:nsites, each = 9),
+                  year = rep(1:9, nsites), 
                   elev = rep(crossbill$ele, each = 9), 
                   forest = rep(crossbill$forest, each = 9), 
                   surveys = rep(crossbill$surveys, each = 9))
@@ -49,9 +50,38 @@ y3.prob.state1.(Intercept)
 m0 <- HMM$new(file = "cross.hmm")
 m0$fit()
 m0$par()
- 
-# effect of forest on turnover
-m_f <- update(m0, "hidden", i = 1, j = 2, ~.+s(forest, bs = "cs", k = 5))
-AIC(m0, m_f)
+
+## I am going to assume detection is constant across time and 
+## all other covariates. 
+
+## Effects on occupancy 
+# effect of year on colonisation
+mt <- update(m0, "hidden", i = 1, j = 2, ~.+s(year, bs = "cs", k = 9))
+# effect of year on extinction
+mt2 <- update(mt, "hidden", i = 2, j = 1, ~.+s(year, bs = "cs", k = 9))
+AIC(m0, mt, mt2)
+
+# effect of forest on colonisation  
+m_f <- update(mt2, "hidden", i = 1, j = 2, ~.+s(forest, bs = "cs"))
+# effect of forest on extinction
+m_f2 <- update(m_f, "hidden", i = 2, j = 1, ~.+s(forest, bs = "cs"))
+AIC(m0, m_f, m_f2)
+
+# effect of elevation on colonisation
+m_e <- update(m_f2, "hidden", i = 1, j = 2, ~.+s(elev, bs = "cs"))
+# effect of elevation on extinction
+m_e2 <- update(m_e, "hidden", i = 2, j = 1, ~.+s(elev, bs = "cs"))
+AIC(m_e2, m_e, m_f2)
+
+# interaction between elevation and forest
+m_int <- update(mt2, "hidden", i = 1, j = 2, ~.+t2(forest, elev, bs = "cs"))
+m_int$fit()
+AIC(m_e, m_int)
+
+m_e2$plot("tpm", "forest")
+m_e2$plot("tpm", "elev")
+m_e2$plot("delta", "forest", i = 2)
+m_e2$plot("delta", "elev", i = 2)
+
 
 
