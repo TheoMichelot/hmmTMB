3ac56b76827f5e71255728048ebb59b9291b756b
Théo Michelot <tm75@st-andrews.ac.uk>
2022-05-20 11:04:21 +0100

    fix driver for new HMM argument list

--- inst/devel/driver.R
+++ inst/devel/driver.R
@@ -5,10 +5,10 @@ library(hmmTMB)
 # simulate data 
 n <- 10000
 empty <- data.frame(ID = 1, count = rep(0, n))
-true_mod <- HMM$new("inst/devel/true_mod.hmm")
+true_mod <- HMM$new(file = "inst/devel/true_mod.hmm")
 dat <- true_mod$simulate(n)
 
 # fit model 
-mod <- HMM$new("inst/devel/mod.hmm")
+mod <- HMM$new(file = "inst/devel/mod.hmm")
 mod$fit()
 mod$par()
