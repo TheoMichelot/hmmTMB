6e3efb9d658e8a62c606fd63ad933925ea9b313d
Hassen Allegue <hallegue@users.noreply.github.com>
2025-10-21 14:47:49 -0300

    Replace size argument by linewidth in ggplot call
    Using `size` aesthetic for lines (geom_line()) was deprecated in ggplot2 3.4.0. Use `linewidth` instead.

--- R/observation.R
+++ R/observation.R
@@ -936,7 +936,7 @@ Observation <- R6Class(
         geom_histogram(breaks = breaks, aes(y = after_stat(density)), 
                        col = "white", bg = "lightgrey", na.rm = TRUE) + 
         geom_line(aes(grid, val, col = state, linetype = state), 
-                  data = df_dens, size = 0.7) +
+                  data = df_dens, linewidth = 0.7) +
         scale_color_manual("", values = c(hmmTMB_cols[1:self$nstates()], "black")) +
         scale_linetype_manual("", values = c(rep(1, self$nstates()), 2)) +
         coord_cartesian(ylim = c(0, 1.1 * max(h$density))) +
